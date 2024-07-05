8a4330ba9247ffdd734e7b9170466e3dca1bfc84
Landon Dyken <ldyken53@uab.edu>
2024-07-05 01:50:45 -0500

    Update

--- start_server.sh
+++ start_server.sh
@@ -9,5 +9,4 @@ cp ml-models/noof640-ultraminiv12.onnx dist/
 cp ml-models/noof1280-ultraminiv12.onnx dist/
 cp ml-models/noof1920-ultraminiv12.onnx dist/
 cd dist/
-export QT_QPA_PLATFORM=offscreen
 python -m http.server
